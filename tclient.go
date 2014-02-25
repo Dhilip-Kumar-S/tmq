87e7cf66b0788d96169eaf953621f5cff0f7a0a4
Dhilip-Kumar-S <dhiliplila@gmail.com>
2014-02-25 20:06:10 +0530

    tcp client/server
    fixed the messaging serialization

--- tclient.go
+++ tclient.go
@@ -1,42 +1,53 @@
 package main
 
 import (
-"fmt"
-"net"
-"encoding/binary"
-"bytes"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"os"
+
+//"time"
 )
 
 type MSG struct {
-	otype	int32
-	mlen	int32
-	m		[]byte	
+	otype int32
+	mlen  int32
+	m     []byte
 }
 
-func main () {
+func main() {
 
-	var err error
-	c, err := net.Dial ("tcp", "[192.168.1.7]:6060")
-	
+	var ip string
+	if len(os.Args) > 1 {
+		ip = os.Args[1]
+	} else {
+		ip = "127.0.0.1"
+	}
+	c, err := net.Dial("tcp", "["+ip+"]:6060")
+
 	if err != nil {
 		fmt.Println("Unable to Dial:", err)
-		panic ("Unable to connect")
+		panic("Unable to connect")
 	}
-	
+
 	var m MSG
-	
-	m.otype = 5
+
+	m.otype = 5000
 	m.mlen = 32
-	m.m = make ([]byte, 32)
-	
-	for i:=0; i<32; i++ {
+	m.m = make([]byte, 32)
+
+	for i := 0; i < 32; i++ {
 		m.m[i] = 0x41
 	}
-	
-	buf := new (bytes.Buffer)
-	err = binary.Write (buf, binary.BigEndian, m.otype)
-	err = binary.Write (buf, binary.BigEndian, m.mlen)
-	m.m = append (buf.Bytes(), m.m...)
-	c.Write (m.m)
+	for {
+
+		buf := new(bytes.Buffer)
+		err = binary.Write(buf, binary.BigEndian, m.otype)
+		err = binary.Write(buf, binary.BigEndian, m.mlen)
+		smg := append(buf.Bytes(), m.m...)
+		c.Write(smg)
+		//time.Sleep (time.Millisecond)
+	}
 	c.Close()
 }
--- tserver.go
+++ tserver.go
@@ -1,64 +1,137 @@
 package main
 
 import (
-"fmt"
-"net"
-"encoding/binary"
-"bytes"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+	"runtime"
+	"time"
 )
 
 type MSG struct {
-	otype	int32
-	mlen	int32
-	m		[]byte	
+	otype int32
+	mlen  int32
+	m     []byte
 }
 
+var tps uint64
 
-func HandleConnection (c net.Conn) {
-	
-	var v MSG
-	
-	var m []byte
-	
-	m = make([]byte, 1024)
-	
-	cnt, err := c.Read(m)
-	
-	fmt.Printf("Recived %d bytes %v\n", cnt, err)
-	
-	bf := bytes.NewBuffer(m)
-	
-	binary.Read(bf, binary.BigEndian, &(v.otype))
-	
-	binary.Read(bf, binary.BigEndian, &(v.mlen))
-	
-	fmt.Printf("Message Type=%d Mlen=%d %s\n", v.otype, v.mlen, string(m[8:cnt]))
-	
-	c.Close ()
-	
-}
-
-func main () {
-	
-	l, err := net.Listen("tcp", "127.0.0.1:6060")
-	if err != nil {
-		fmt.Println ("Listen Failed:", err)
-		panic ("Server failed to bind:")
-	}
-	fmt.Printf("Listen sucessful:", l)
-	
+func printstats() {
 	for {
-		
+		select {
+		case <-time.After(1 * time.Second):
+			fmt.Printf("tps = %v\n", tps)
+			tps = 0
+
+		}
+	}
+}
+
+func DispStts(s chan int) {
+
+	go printstats()
+	for {
+		select {
+		case <-s:
+			tps++
+		}
+	}
+}
+
+/*
+
+func ReadN (c net.Conn, n int) ([]byte, error, int) {
+        msg := make ([]byte, n)
+
+        cnt , err := c.Read(msg)
+        if err != nil {
+                return msg,err, cnt
+        }
+        if cnt == n {
+                return msg, nil, cnt
+        }
+        tcnt := cnt
+        for tcnt == n {
+                cnt, err := c.Read(msg[tcnt:])
+                if err != nil {
+                        return msg,err, tcnt
+                }
+                tcnt += cnt
+        }
+        return msg,nil,tcnt
+
+}
+*/
+
+func HandleConnection(c net.Conn, s chan int) {
+
+	for {
+		var v MSG
+
+		var m []byte
+
+		m = make([]byte, 40)
+
+		debug := false
+
+		cnt, err := io.ReadAtLeast(c, m, 40)
+
+		if err != nil {
+			fmt.Println("ReadAtleast :", err)
+			break
+		}
+
+		if cnt != 40 {
+			fmt.Println("Bad message len:", cnt)
+			break
+		}
+
+		if debug {
+			fmt.Printf("Recived %d bytes %v\n", cnt, err)
+		}
+
+		bf := bytes.NewBuffer(m)
+
+		binary.Read(bf, binary.BigEndian, &(v.otype))
+
+		binary.Read(bf, binary.BigEndian, &(v.mlen))
+
+		s <- 1
+		if debug {
+			fmt.Printf("Message Type=%d Mlen=%d %s\n", v.otype, v.mlen, string(m[8:cnt]))
+		}
+
+	}
+
+	c.Close()
+
+}
+
+func main() {
+
+	l, err := net.Listen("tcp", ":6060")
+	if err != nil {
+		fmt.Println("Listen Failed:", err)
+		panic("Server failed to bind:")
+	}
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	s := make(chan int)
+
+	go DispStts(s)
+
+	for {
+
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Accep failed:", err)
 		}
-		
-		go HandleConnection (conn)
-		
+
+		go HandleConnection(conn, s)
+
 	}
-	
+
 }
-
-
-
